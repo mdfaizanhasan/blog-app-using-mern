8da9b92c6859d6b363c87c1017fda69b36d469b8
faizanhasan <mdfaizanalam367@gmail.com>
2024-01-25 23:10:31 +0530

    add auth functionality

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -16,6 +16,10 @@ const userSchema = new mongoose.Schema(
             type: String,
             required: true,
         },
+        profilePicture: {
+            type: String,
+            default: 'https://i.pinimg.com/originals/47/31/03/473103',
+        },
     }, { timestamps: true }
 );
 
