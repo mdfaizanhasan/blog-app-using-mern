0ef30294a0a0ade51be62f34edc5b9eb4d874f18
faizanhasan <mdfaizanalam367@gmail.com>
2024-02-07 19:04:40 +0530

    Add delete user api route

--- server/utils/verifyUser.js
+++ server/utils/verifyUser.js
@@ -8,7 +8,7 @@ export const verifyToken = (req, res, next) => {
     }
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
         if (err) {
-            return next(errorHandler(401, 'Unauthorized'));
+            return next(errorHandler(401, 'Unauthorized User Not Found!'));
         }
         req.user = user;
         next();
